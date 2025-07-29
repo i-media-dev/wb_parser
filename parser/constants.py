593b287211c0589570fbb14de7a6c7d492e00e35
i-media-dev <mrk_dev@i-media.ru>
2025-07-29 18:50:12 +0300

    fix second test actions

--- parser/constants.py
+++ parser/constants.py
@@ -1,6 +1,9 @@
 """URLs для получения данных из внешнего API"""
 
-WB_PRODUCT_DATA = 'https://seller-analytics-api.wildberries.ru/api/v2/stocks-report/products/products'
+WB_PRODUCT_DATA = (
+    'https://seller-analytics-api.wildberries.ru/'
+    'api/v2/stocks-report/products/products'
+)
 WB_AVG_SALES = 'https://statistics-api.wildberries.ru/api/v1/supplier/orders'
 
 """Константы, регулирующие запрос"""
